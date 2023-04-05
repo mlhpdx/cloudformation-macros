a05b7025885e7f20bca6c722379064c106f07e2f
Lee Harding <lee.harding@circuitpeople.com>
2023-04-05 08:42:57 -0700

    Use sam.

--- scripts/deploy-global.sh
+++ scripts/deploy-global.sh
@@ -3,5 +3,5 @@ set -e
 
 # global resources deploy only in us-west-2!
 
-aws cloudformation package --template-file templates/cfn-macros.template --s3-bucket ${BUCKET_NAME_PREFIX}-us-west-2 --s3-prefix ${BUCKET_KEY_PREFIX}/email-service/${CODEBUILD_RESOLVED_SOURCE_VERSION} --region us-west-2 > cfn-macros.template.published
-aws cloudformation deploy --stack-name cfn-macros --template-file cfn-macros.template.published --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region us-west-2
+aws sam package --template-file templates/cfn-macros.template --s3-bucket ${BUCKET_NAME_PREFIX}-us-west-2 --s3-prefix ${BUCKET_KEY_PREFIX}/email-service/${CODEBUILD_RESOLVED_SOURCE_VERSION} --region us-west-2 > cfn-macros.template.published
+aws sam deploy --stack-name cfn-macros --template-file cfn-macros.template.published --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region us-west-2
