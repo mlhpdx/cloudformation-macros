976ef2a6b8cd5e3ccda6f21481025ee155ecfb5c
Lee Harding <lee.harding@circuitpeople.com>
2024-06-05 14:36:23 -0700

    Deploy to all enabled regions.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,21 @@
 # exit when any command fails
-set -e
+set -e -x
 
 sam build --template-file templates/cfn-macros.template
 
-sam deploy --s3-bucket ${BUCKET_NAME_PREFIX}-us-west-2 --s3-prefix ${BUCKET_KEY_PREFIX}/email-service/${CODEBUILD_RESOLVED_SOURCE_VERSION} --stack-name cfn-macros --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region us-west-2
-sam deploy --s3-bucket ${BUCKET_NAME_PREFIX}-us-east-1 --s3-prefix ${BUCKET_KEY_PREFIX}/email-service/${CODEBUILD_RESOLVED_SOURCE_VERSION} --stack-name cfn-macros --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region us-east-1
-sam deploy --s3-bucket ${BUCKET_NAME_PREFIX}-eu-west-1 --s3-prefix ${BUCKET_KEY_PREFIX}/email-service/${CODEBUILD_RESOLVED_SOURCE_VERSION} --stack-name cfn-macros --no-fail-on-empty-changeset --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region eu-west-1
+# deploy to all of the regions enabled in the account
+export ACCOUNT_REGIONS=$(aws account list-regions \
+  --region-opt-status-contains "ENABLED" "ENABLED_BY_DEFAULT" \
+  --no-paginate \
+  --query "Regions[].RegionName" \
+  --output text)
+
+for region in $ACCOUNT_REGIONS; do
+  sam deploy \
+  --s3-bucket ${BUCKET_NAME_PREFIX}-${region} \
+  --s3-prefix ${BUCKET_KEY_PREFIX}/email-service/${CODEBUILD_RESOLVED_SOURCE_VERSION} \
+  --stack-name cfn-macros \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
+  --region $region
+done
